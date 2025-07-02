7dc5c66181ab7ab3aecde337f27dce0d1795c7ed
harshiit-rana <ranaharshit633@gmail.com>
2025-07-02 18:25:30 +0530

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -73,5 +73,8 @@ def weather_api():
 
     return jsonify(weather)
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
+
